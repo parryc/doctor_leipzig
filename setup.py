f7cc7fea22447e1a1d49954dbef97d093f71b027
parry <parry@parryc.com>
2020-12-25 14:18:48 -0600

    Update to support Python Markdown 3.3.3
    Changes:
     - Run black formatter over code
     - Remove some cruft in the code
     - Update to use Python Markdown 3.0+ extension API

--- setup.py
+++ setup.py
@@ -1,24 +1,23 @@
 from setuptools import setup
-import os
 
 setup(
-  name='doctor_leipzig',
-  version='1.1',
-  description='Interlinear (Leipzig Rules) glossing for Markdown',
-  long_description='Doctor Lepizig is a set of additional Markdown syntax items that help with constructing interlinear glosses within Markdown pages. Please see the README or website for full API.',
-  url='https://github.com/parryc/doctor_leipzig',
-  py_modules=['doctor_leipzig'],
-  packages=['doctor_leipzig'],
-  license='MIT',
-  author='parryc',
-  author_email='parry@parryc.com',
-  keywords='interlinear leipzig glossing gloss markdown',
-  install_requires=['markdown>=2.5'],
-  classifiers=[
-    'Intended Audience :: Developers',
-    'Programming Language :: Python',
-    'Topic :: Software Development :: Libraries :: Python Modules',
-    'Topic :: Text Processing :: Filters',
-    'Topic :: Text Processing :: Markup :: HTML'
-    ]
+    name="doctor_leipzig",
+    version="2.0",
+    description="Interlinear (Leipzig Rules) glossing for Markdown",
+    long_description="Doctor Lepizig is a set of additional Markdown syntax items that help with constructing interlinear glosses within Markdown pages. Please see the README or website for full API.",
+    url="https://github.com/parryc/doctor_leipzig",
+    py_modules=["doctor_leipzig"],
+    packages=["doctor_leipzig"],
+    license="MIT",
+    author="parryc",
+    author_email="parry@parryc.com",
+    keywords="interlinear leipzig glossing gloss markdown",
+    install_requires=["markdown>=3.3.3"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing :: Filters",
+        "Topic :: Text Processing :: Markup :: HTML",
+    ],
 )
